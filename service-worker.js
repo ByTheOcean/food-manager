2421cf5b4414d3249ee73b6fa2c7498d4ea93fd7
ByTheOcean <pb9801+git@gmail.com>
2025-10-05 00:55:23 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'food-manager-v1.0';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// 安装Service Worker
+self.addEventListener('install', (event) => {
+  console.log('Service Worker 安装中...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => {
+        return self.skipWaiting();
+      })
+  );
+});
+
+// 激活Service Worker
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker 激活中...');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
+
+// 拦截请求
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
